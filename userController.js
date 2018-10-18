f81a3a8efab12ac7e3e84df999c342e888185543
MatheusHALeal <matheus.leal@ccc.ufcg.edu.br>
2018-10-18 17:50:13 -0300

    Add files via upload

--- userController.js
+++ userController.js
@@ -1,59 +1,82 @@
-user.controller('userCtrl',function ($scope,$http) {
-$scope.apiKey = '6cdc036afa25edf9ffa2f4b26794ebc5';
+user.controller('userCtrl', function ($scope,$http) {
 
+$scope.apiKey = '6cdc036afa25edf9ffa2f4b26794ebc5';
 $scope.vizinhos = [];
-$scope.targetTags = [];
-$scope.sourceTags = [];
+
 $scope.getTopArtists = function(username){
   var url ="http://ws.audioscrobbler.com/2.0/?method=user.gettopartists&user="+username+"&api_key="+$scope.apiKey+"&format=json";
 
   $http.get(url).then(function (data) {
     console.log(data);
     var i;
-    for (i = 0; i < 1; i++) {
+    for (i = 0; i < 10; i++) {
       console.log(data.data.topartists.artist[i].name);
-     $scope.getRelatedArtist(data.data.topartists.artist[i].name);
-  }
-  console.log($scope.vizinhos)
+    $scope.getRelatedArtist(data.data.topartists.artist[i].name);
+  }console.log($scope.vizinhos)
 });
 }
 
 
 $scope.getRelatedArtist = function(artist){
   $http.get("http://ws.audioscrobbler.com/2.0/?method=artist.getsimilar&artist="+artist+"&api_key="+$scope.apiKey+"&format=json").then(function (data) {
-    var i;
-    if (data.data.similarartists !== undefined){
-    for (i = 0; i < 10; i++) {
-      if (data.data.similarartists.artist[i] !== undefined) {
-        $scope.vizinhos.push({"source":artist, "target": data.data.similarartists.artist[i].name})
-        $scope.getArtistTags(data.data.similarartists.artist[i].name);
-        console.log(data.data.similarartists.artist[i].name);
+    let sourceTags = $scope.getArtistTags(artist);
+    let similar = (data.data.similarartists.artist)
+    sourceTags.then((array)=>{
+        if (similar !== undefined){
+        for (let i = 0; i < 10; i++) {
+          if (similar[i] !== undefined) {
 
-    }
+            let targetTags = $scope.getArtistTags(similar[i].name);
 
-    }}
+            targetTags.then((response) => {
+              let w = $scope.matches(array, response);
+              $scope.vizinhos.push({"source":artist, "target": similar[i].name, "weight": w})
 
-});
+            })
+        }
+        }}
+    })
+
+  });
 }
 
 
+$scope.matches = function(sourceTags, targetTags) {
+  console.log(sourceTags);
+  var peso = 0;
+  var i;
+    for (i = 0; i < sourceTags.length; i++) {
+        var j;
+        for (j = 0; j < targetTags.length; j++) {
+          if (sourceTags[i].name === targetTags[j].name) {
+              peso += (sourceTags[i].count/100.0) * (targetTags[j].count/100.0)
+          }
+        }
 
+
+    }
+
+  return peso;
+}
 
 
 $scope.getArtistTags = function(artist){
-  $http.get("http://ws.audioscrobbler.com/2.0/?method=artist.gettoptags&artist="+artist+"&api_key="+$scope.apiKey+"&format=json").then(function (data) {
-    var t = []
+  var t = [];
+    return $http.get("http://ws.audioscrobbler.com/2.0/?method=artist.gettoptags&artist="+artist+"&api_key="+$scope.apiKey+"&format=json").then(function (data) {
+
     //var tags = [];
     var i= 0;
-    if (data.toptags.tag !== undefined){
-       while(data.toptags.tag[i] !== undefined){
+    if (data.data.toptags.tag !== undefined){
+       while(data.data.toptags.tag[i] !== undefined){
          var tupla = Object.freeze({ name:data.data.toptags.tag[i].name, count:data.data.toptags.tag[i].count})
-         $scope.tar.push(tupla)
+         t.push(tupla)
          i++;
        }
     }
-    data = t;
+      return t;
   })
+
+
 
 };
 
